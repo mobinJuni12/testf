f1372b29658fc2e38b70d026938c81659f7b7195
mobinJuni12 <146773426+mobinJuni12@users.noreply.github.com>
2024-12-10 12:44:55 -0800

    Update a.py

--- folder/a.py
+++ folder/a.py
@@ -1,1 +1,24 @@
+import turtle
+t = turtle.Turtle()
 
+t.pendown()
+t.color('blue')
+for i in range(4):
+  t.forward(100)
+  t.right(90)
+t.penup()
+
+t.goto(-150,150)
+t.pendown()
+t.color('red')
+for i in range(3):
+  t.forward(100)
+  t.right(120)
+t.penup()
+
+t.goto(-150,-100)
+t.pendown()
+t.color('green')
+for i in range(36):
+  t.forward(10)
+  t.right(10)
